45fc01b8573a2509cf98395abf5919d8be74615e
Ban-Takumi <er22059-1823@sti.chubu.ac.jp>
2026-06-25 18:52:05 +0900

    feat(widget): WidgetKitを利用したホーム画面ウィジェットの実装

---
+++ FocusTimer/FocusTimerWidget/FocusTimerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  FocusTimerWidgetBundle.swift
+//  FocusTimerWidget
+//
+//  Created by Takumi Ban on 2026/06/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FocusTimerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FocusTimerWidget()
+    }
+}
